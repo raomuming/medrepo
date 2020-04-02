f5404ee1004b9e80f235af9166dab3b4ced6fa65
eric.rao <eric.rao@ringcentral.com>
2020-04-02 22:03:42 +0800

    home page.

---
+++ src/pages/tools/editor/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class wysiwyg extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
---
+++ src/pages/tools/smallTools/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Tools = () => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Tools;
