c35eb76a67a341a78c99ca8e83495ff56b204701
eric.rao <eric.rao@ringcentral.com>
2020-04-17 00:21:47 +0800

    question tabs.

--- miniprogram/pages/questionBank/questionBank.js
+++ miniprogram/pages/questionBank/questionBank.js
@@ -2,4 +2,12 @@
 import Page from '../../common/page';
 
 Page({
+  data: {
+    tabsWithName: [
+      { name: 'a', index: 1, title: '学科真题' },
+      { name: 'b', index: 2, title: '必背考点' },
+      { name: 'c', index: 3, title: '随机组题' },
+      { name: 'd', index: 4, title: '模拟考试' }
+    ]
+  }
 });
