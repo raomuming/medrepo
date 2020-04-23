c5b0e63374a9e77c36876b9b6b390eab2f702875
eric.rao <eric.rao@ringcentral.com>
2020-04-23 22:40:10 +0800

    question grid.

---
+++ miniprogram/pages/questionGrid/questionGrid.js
@@ -0,0 +1,8 @@
+// miniprogram/pages/questionGrid/questionGrid.js
+import Page from '../../common/page';
+
+Page({
+  data: {
+    questions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+  }
+});
