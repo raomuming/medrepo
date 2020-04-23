4ba71d19181f95ed5c189082d43b886dd16855db
eric.rao <eric.rao@ringcentral.com>
2020-04-23 22:57:32 +0800

    navigate to question detail.

--- miniprogram/pages/questionGrid/questionGrid.js
+++ miniprogram/pages/questionGrid/questionGrid.js
@@ -4,5 +4,10 @@ import Page from '../../common/page';
 Page({
   data: {
     questions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+  },
+  onClick: function() {
+    wx.navigateTo({
+      url: '../questionDetail/questionDetail',
+    });
   }
 });
